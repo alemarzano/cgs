9a984b8173f94436e8a44b1726c6ada74a722579
alegroni <ajmarzano@gmail.com>
2020-06-11 04:37:19 -0300

    Incoterms terminados

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -176,50 +176,6 @@ if (hash != '') {
     window.scrollTo(0,0);
 }
 
-<<<<<<< HEAD
-// Change hash for page-reload
-$('.nav-pills a').on('shown.bs.tab', function (e) {
-    window.location.hash = e.target.hash;
-})
-
-// SMOOTH SCROLL //
-
-$('a.anchor[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - 90
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-=======
 // SMOOTH SCROLL //
 
 $('a.anchor[href*="#"]')
@@ -256,4 +212,3 @@ $('a.anchor[href*="#"]')
             }
         }
     });
->>>>>>> ed9deaf8924f22d6f18fa1b2e5e72093b8cb48a3
